26def183c0009077432451796cb9294d9afaf12c
Kromag1995 <gonzalof95@hotmail.com>
2022-03-11 00:00:54 -0300

    Add caousel news

--- models.py
+++ models.py
@@ -12,15 +12,23 @@ class Site(Base):
     url = Column(String(100))
     name = Column(String(50))
     main_news_id = Column(Integer, default=-1)
-    news = relationship("News", back_populates="site")
 
-class News(Base):
-    __tablename__ = "news"
+class MainNews(Base):
+    __tablename__ = "mainnews"
 
     id = Column(Integer,primary_key=True)
     url = Column(String(100))
-    is_main = Column(Boolean, default=True)
     start = Column(DateTime)
     stop = Column(DateTime, default=datetime(1, 1, 1, 0, 0))
     site_id = Column(Integer, ForeignKey("site.id"))
-    site = relationship("Site", back_populates="news")
+    is_main = Column(Boolean, default=True)
+
+class CoureselNews(Base):
+    __tablename__ = "carouselnews"
+
+    id = Column(Integer,primary_key=True)
+    url = Column(String(100))
+    start = Column(DateTime)
+    stop = Column(DateTime, default=datetime(1, 1, 1, 0, 0))
+    site_id = Column(Integer, ForeignKey("site.id"))
+    is_carousel = Column(Boolean, default=False)
